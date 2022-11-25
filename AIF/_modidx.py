35e30ec1c66f7fe5fb8b6be9c4c88f840583affe
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2022-11-25 10:47:26 +0000

    Init version

--- AIF/_modidx.py
+++ AIF/_modidx.py
@@ -5,4 +5,6 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://erling6232.github.io',
                 'git_url': 'https://github.com/erling6232/AIF',
                 'lib_path': 'AIF'},
-  'syms': {'AIF.core': {'AIF.core.foo': ('core.html#foo', 'AIF/core.py')}}}
+  'syms': { 'AIF.AIF': {'AIF.AIF.foo': ('core.html#foo', 'AIF/AIF.py')},
+            'AIF.core': {'AIF.core.foo': ('parker.html#foo', 'AIF/core.py')},
+            'AIF.parker': {'AIF.parker.foo': ('parker.html#foo', 'AIF/parker.py')}}}
